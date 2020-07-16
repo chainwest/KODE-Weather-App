38e4038967a6458764b9d661fcceee4694c0e965
chainwest <evgeniyurbanovskiy@gmail.com>
2020-07-16 15:13:18 +0700

    Base code

---
+++ KODE Weather App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  KODE Weather App
+//
+//  Created by Evgeniy on 16.07.2020.
+//  Copyright © 2020 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'KODE Weather App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for KODE Weather App
+  pod 'Alamofire'
+  pod 'Kingfisher'
+  pod 'SwiftLint'
+
+end
